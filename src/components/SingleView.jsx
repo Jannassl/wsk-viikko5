da69278e8eba1ad1fbd03199ec5b4fcaefc48618
Jannassl <Jan.nassling@metropolia.fi>
2024-04-15 14:04:48 +0300

    useRef

--- src/components/SingleView.jsx
+++ src/components/SingleView.jsx
@@ -1,27 +1,52 @@
 import PropTypes from 'prop-types';
+import { useEffect, useRef } from 'react';
 
 const SingleView = (props) => {
   const { setSelectedItem,selectedItem} = props;
+  const modal = useRef(null);
   const handleClick = () => {
     setSelectedItem(null);
   }
+
+  useEffect(() => {
+    if(selectedItem){
+      modal.current.showModal();
+    }else{
+      modal.current.close();
+    }
+  }, [selectedItem]);
   return (
     <>
-      <dialog open = {selectedItem ? true : false}>
+      <dialog ref={modal}>
         <p>
           <button onClick={handleClick}>Close</button>
         </p>
         {selectedItem && (
+          <>
+          {selectedItem.media_type.includes('video') ? (
+          <video controls>
+          <source src={selectedItem.filename} type= {selectedItem.media_type} />
+          </video>
+
+          ):(
           <img src={selectedItem.filename} alt={selectedItem.title} />
           )}
+          <h2>{selectedItem.title}</h2>
+          <p>{selectedItem.desription}</p>
+          <p>{new Date(selectedItem.created_at).toLocaleString('fi-FI')}</p>
+          <p>{selectedItem.filesize}</p>
+          <p>{selectedItem.media_type}</p>
+        </>
+        )}
       </dialog>
     </>
   );
 };
 
 SingleView.propTypes = {
-  selectedItem: PropTypes.func.isRequired,
-  setSelectedItem: PropTypes.func.isRequired
+  selectedItem: PropTypes.object.isRequired,
+  setSelectedItem: PropTypes.func.isRequired,
+  useRef: PropTypes.func
 };
 
 
