cb60fcbf7bc24ac4e8dea234555751f926534dac
Jannassl <Jan.nassling@metropolia.fi>
2024-04-22 11:17:11 +0300

    Lab2 tehty

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -3,22 +3,32 @@ import MediaRow from '../components/mediaRow';
 import { useEffect, useState } from 'react';
 import {fetchData} from '../lib/fetchData';
 
-
-
-
-
 const Home = () => {
   //const [selectedItem, setSelectedItem] = useState(null);
   const [mediaArray, setMediaArray] = useState([]);
 
   const getMedia = async () => {
-    const mediaResult = await fetchData('test.json');
-    setMediaArray(mediaResult);
+    const mediaResult = await fetchData(
+      import.meta.env.VITE_MEDIA_API + '/media'
+    );
+
+    const mediaWithUser =await Promise.all(mediaResult.map(async(mediaItem) => {
+      const userResult = await fetchData(
+        import.meta.env.VITE_AUTH_API+ '/users/'+ mediaItem.user_id,
+    );
+    return {...mediaItem ,username: userResult.username};
+
+  }));
+
+  console.log(mediaWithUser);
+    setMediaArray(mediaWithUser);
   };
 
   useEffect(() => {
     getMedia();
   }, []);
+
+
 
 
   return (
@@ -28,6 +38,7 @@ const Home = () => {
         <thead>
           <tr>
             <th>Thumbnail</th>
+            <th>Owner</th>
             <th>Title</th>
             <th>Description</th>
             <th>Created</th>
